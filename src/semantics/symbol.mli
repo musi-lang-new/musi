b6fc4363bf0aa6cf647b5e9c377b938c245ec78d
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-28 06:10:59 +0200

    feat: add `symbol` type and related functions for symbol management in `symbol.mli`

---
+++ src/semantics/symbol.mli
@@ -0,0 +1,27 @@
+type symbol_kind =
+  | Bind of { mutable_ : bool; typ : Musi_syntax.Tree.typ option }
+  | Proc of {
+        params : Musi_syntax.Tree.param list
+      ; ret_typ : Musi_syntax.Tree.typ option
+    }
+  | Type of { fields : Musi_syntax.Tree.field list option }
+
+type symbol = {
+    name : Musi_shared.Interner.symbol
+  ; kind : symbol_kind
+  ; span : Musi_shared.Span.t
+}
+
+type t = {
+    curr_scope : scope ref
+  ; diags : Musi_shared.Diagnostic.diagnostic_bag ref
+  ; interner : Musi_shared.Interner.t
+}
+
+and scope
+
+val create : Musi_shared.Interner.t -> t
+val enter_scope : t -> unit
+val exit_scope : t -> unit
+val lookup : t -> Musi_shared.Interner.symbol -> symbol option
+val define : t -> symbol -> unit
