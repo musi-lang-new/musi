b7f1c3a14c0f1bd4bdee555bce5fe25e9cacd12f
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-29 04:19:36 +0200

    style: improve formatting and comments in `compiler.mli`

--- musi_driver/compiler.mli
+++ musi_driver/compiler.mli
@@ -1,7 +1,11 @@
-type result =
-  | Success of Instr.program
-  | Failure of Diagnostic.diagnostic_bag
+(** Compilation result *)
+type result = Success of Instr.program | Failure of Diagnostic.diagnostic_bag
 
+(** Compile source file to bytecode file *)
 val compile_file : string -> string -> result
+
+(** Compile source string to bytecode program *)
 val compile_string : string -> result
+
+(** Print diagnostics to stderr with source context *)
 val print_diagnostics : Diagnostic.diagnostic_bag -> string -> unit
