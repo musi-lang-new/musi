ada4a778e4d3f5f3daa43d65f6df54ecd8e48440
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-29 14:41:04 +0200

    refactor: remove `binder.ml` and `binder.mli` files to clean up codebase

--- musi_fe/binder.mli
+++
@@ -1,9 +0,0 @@
-(** Binder state *)
-type t = { syms : Symbol.t; interner : Interner.t }
-
-(** Create binder with interner *)
-val create : Interner.t -> t
-
-(** Bind program and return diagnostics *)
-val bind_program :
-  t -> Tree.program -> Diagnostic.diagnostic_bag
