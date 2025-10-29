7582dd1d5657cb2e7088e25cafd072184a79a17b
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-29 14:14:33 +0200

    feat: enhance `node_kind` and `ty` types with new expressions and modifiers

--- musi_fe/tree.mli
+++ musi_fe/tree.mli
@@ -1,67 +1,111 @@
 type trivia = Token.token list
 type ty_ref = int
 type symbol_ref = int
+type abi = Interner.symbol
+type 'a separated = { items : 'a list; separators : Span.t list }
 
-type modifiers = {
-    exported : bool
-  ; weakness : bool
-  ; constness : bool
-  ; unsafeness : bool
-  ; asyncness : bool
-  ; externness : bool * Interner.symbol option
+type 'a delimited = {
+    open_span : Span.t
+  ; items : 'a list
+  ; separators : Span.t list
+  ; close_span : Span.t
 }
 
-val default_modifiers : modifiers
-
-type ty_kind =
-  | Named of { name : Interner.symbol }
-  | Proc of { param_tys : ty list; ret_ty : ty }
-  | Optional of { inner_ty : ty }
-  | Fallible of { pass_ty : ty; fail_ty : ty option }
-  | Array of { elem_ty : ty }
-  | ArrayRepeat of { elem_ty : ty; count_ty : ty }
-  | Tuple of { elem_tys : ty list }
-  | Generic of { name : Interner.symbol; args : ty list }
-  | Where of { ty : ty; constraints : constraint_ list }
-  | Ptr of { inner : ty }
-  | Ref of { inner : ty }
-  | Infer
+type node_kind =
+  (* Expr: Literals *)
+  | ExprIntLit of { value : string; suffix : Token.suffix option }
+  | ExprBinLit of { value : string; suffix : Token.suffix option }
+  | ExprTextLit of { value : Interner.symbol }
+  | ExprBoolLit of { value : bool }
+  | ExprUnitLit
+  | ExprIdent of { name : Interner.symbol }
+  (* Expr: Operators *)
+  | ExprBinary of { op : Token.t; lhs : node; rhs : node }
+  | ExprUnary of { op : Token.t; operand : node }
+  | ExprAssign of { lhs : node; rhs : node }
+  (* Expr: Calls & Access *)
+  | ExprCall of { callee : node; args : node delimited }
+  | ExprField of { receiver : node; field : Interner.symbol }
+  | ExprIndex of { receiver : node; index : node }
+  (* Expr: Control Flow *)
+  | ExprIf of { cond : node; then_br : node; else_br : node option }
+  | ExprMatch of { scrutinee : node; cases : match_case separated }
+  | ExprWhile of { cond : node; body : node }
+  | ExprFor of { pat : node; iterable : node; body : node }
+  | ExprBlock of { body : node separated; unsafe_ : bool; asyncness : bool }
+  | ExprReturn of { value : node option }
+  | ExprBreak of { value : node option }
+  | ExprContinue
+  (* Expr: Compound *)
+  | ExprArray of { items : node delimited }
+  | ExprArrayList of { item : node; count : node }
+  | ExprTuple of { items : node delimited }
+  | ExprRecordLit of { fields : record_field delimited }
+  (* Expr: Definitions *)
+  | ExprProc of {
+        params : param delimited
+      ; ret_ty : ty option
+      ; body : node option
+      ; asyncness : bool
+      ; unsafe_ : bool
+      ; external_ : abi option
+    }
+  | ExprRecord of {
+        fields : record_field_def delimited
+      ; methods : node list
+      ; unsafe_ : bool
+    }
+  | ExprChoice of { variants : choice_variant delimited; methods : node list }
+  | ExprInterface of { signatures : node delimited; unsafe_ : bool }
+  | ExprBind of {
+        mutable_ : bool
+      ; weakness : bool
+      ; pat : node
+      ; ty : ty option
+      ; init : node
+    }
+  (* Expr: Misc *)
+  | ExprTry of { inner : node }
+  | ExprDefer of { inner : node }
+  | ExprRange of { start : node; end_ : node; inclusive : bool }
+  | ExprCast of { inner : node; target : ty }
+  | ExprTest of { inner : node; target : ty }
+  | ExprImport of { source : Interner.symbol; kind : import_export_kind }
+  | ExprExport of { source : Interner.symbol option; kind : import_export_kind }
+  (* Pat: Patterns *)
+  | PatWildcard
+  | PatBind of { inner : node }
+  | PatOr of { alts : node separated }
+  | PatRest of { name : Interner.symbol option }
+  | PatExpr of { inner : node }
+  (* Error *)
   | Error
 
-and ty = {
-    kind : ty_kind
+and node = {
+    kind : node_kind
   ; span : Span.t
   ; leading : trivia
   ; trailing : trivia
-}
-
-and pat_kind =
-  | Wildcard
-  | Ident of { name : Interner.symbol }
-  | IntLit of { value : string }
-  | BoolLit of { value : bool }
-  | TextLit of { value : Interner.symbol }
-  | Tuple of { pats : pat list }
-  | Record of { fields : (Interner.symbol * pat) list }
-  | Array of { pats : pat list }
-  | Range of { start : pat; end_ : pat; inclusive : bool }
-  | Choice of { member : Interner.symbol; pat : pat option }
-  | Or of { pats : pat list }
-  | Rest of { name : Interner.symbol option }
-  | ValueBinding of { name : Interner.symbol; pat : pat }
-  | Error
-
-and pat = {
-    kind : pat_kind
-  ; span : Span.t
-  ; leading : trivia
-  ; trailing : trivia
+  ; decorators : decorator list
+  ; exported : bool
   ; mutable ty : ty_ref option
+  ; mutable sym : symbol_ref option
 }
 
-and template_part =
-  | Text of { value : Interner.symbol }
-  | Expr of { expr : expr }
+and ty_kind =
+  | TyNamed of { name : Interner.symbol }
+  | TyProc of { params : ty list; ret : ty }
+  | TyOptional of { inner : ty }
+  | TyFallible of { ok : ty; err : ty option }
+  | TyArray of { item : ty }
+  | TyArrayList of { item : ty; count : node }
+  | TyTuple of { items : ty list }
+  | TyPtr of { inner : ty }
+  | TyRef of { inner : ty }
+  | TyInfer
+  | TyError
+
+and ty = { kind : ty_kind; span : Span.t; leading : trivia; trailing : trivia }
 
 and param = {
     name : Interner.symbol
@@ -71,111 +115,40 @@ and param = {
   ; trailing : trivia
 }
 
-and expr_kind =
-  | IntLit of { value : string; suffix : Token.suffix option }
-  | BinLit of { value : string; suffix : Token.suffix option }
-  | TextLit of { value : Interner.symbol }
-  | BoolLit of { value : bool }
-  | UnitLit
-  | Ident of { name : Interner.symbol }
-  | Binary of { op : Token.t; lhs : expr; rhs : expr }
-  | Unary of { op : Token.t; operand : expr }
-  | Call of { callee : expr; args : expr list }
-  | If of { cond : expr; then_br : expr; else_br : expr option }
-  | Match of { expr : expr; cases : match_case list }
-  | Block of { stmts : stmt list }
-  | Array of { elems : expr list }
-  | ArrayRepeat of { elem : expr; count : expr }
-  | Tuple of { elems : expr list }
-  | RecordLit of { fields : (Interner.symbol * expr) list }
-  | Record of { fields : field list; methods : stmt list }
-  | Choice of { cases : choice_case list; methods : stmt list }
-  | Interface of { methods : stmt list }
-  | Proc of { params : param list; ret_ty : ty option; body : stmt list option }
-  | Bind of { mutable_ : bool; pat : pat; ty : ty option; init : expr }
-  | Assign of { lhs : expr; rhs : expr }
-  | Return of { value : expr option }
-  | Break of { value : expr option }
-  | Continue
-  | While of { cond : expr; body : stmt list }
-  | For of { pat : pat; iter : expr; body : stmt list }
-  | Field of { receiver : expr; field : Interner.symbol }
-  | Index of { receiver : expr; index : expr }
-  | Try of { expr : expr }
-  | Defer of { expr : expr }
-  | Range of { start : expr; end_ : expr; inclusive : bool }
-  | Async of { expr : expr }
-  | Await of { expr : expr }
-  | Cast of { expr : expr; ty : ty }
-  | Test of { expr : expr; ty : ty }
-  | Template of { parts : template_part list }
-  | Error
-
-and expr = {
-    kind : expr_kind
-  ; span : Span.t
-  ; leading : trivia
-  ; trailing : trivia
-  ; mutable ty : ty_ref option
-  ; mutable sym : symbol_ref option
-}
-
 and match_case = {
-    pat : pat
-  ; guard : expr option
-  ; body : expr
+    pat : node
+  ; guard : node option
+  ; body : node
   ; span : Span.t
   ; leading : trivia
   ; trailing : trivia
 }
 
-and stmt_kind =
-  | Expr of { expr : expr }
-  | Import of { path : Interner.symbol; kind : import_export_kind }
-  | Export of {
-        path : Interner.symbol option
-      ; kind : import_export_kind
-    }
-  | Alias of { name : Interner.symbol; ty : ty }
-  | Error
+and decorator = { name : Interner.symbol; args : node list; span : Span.t }
 
-and stmt = {
-    kind : stmt_kind
+and record_field = {
+    name : Interner.symbol
+  ; value : node
   ; span : Span.t
   ; leading : trivia
   ; trailing : trivia
-  ; decorators : decorator list
-  ; modifiers : modifiers
-  ; mutable sym : symbol_ref option
 }
 
-and field = {
+and record_field_def = {
     name : Interner.symbol
   ; ty : ty
-  ; default_value : expr option
+  ; default : node option
   ; span : Span.t
   ; leading : trivia
   ; trailing : trivia
 }
 
-and choice_case = {
+and choice_variant = {
     name : Interner.symbol
-  ; ty : ty option
+  ; payload : ty option
   ; span : Span.t
   ; leading : trivia
   ; trailing : trivia
-}
-
-and decorator = {
-    name : Interner.symbol
-  ; args : expr list
-  ; span : Span.t
-}
-
-and constraint_ = {
-    intf : Interner.symbol
-  ; args : ty list
-  ; span : Span.t
 }
 
 and import_export_item = {
@@ -187,9 +160,5 @@ and import_export_item = {
 and import_export_kind =
   | Namespace of { alias : Interner.symbol }
   | Named of { items : import_export_item list }
-  | ReExport of {
-        path : Interner.symbol
-      ; items : import_export_item list option
-    }
 
-type program = stmt list
+type program = node list
