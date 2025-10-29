7c913e47d6babb708460129bbca7e9566a4880bc
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-29 14:19:15 +0200

    refactor: tidy up `span.ml` by adding spacing for improved readability

--- musi_shared/span.ml
+++ musi_shared/span.ml
@@ -1,9 +1,25 @@
+(** Source location spans. *)
+
+(** File identifier. *)
 type file_id = int
+
+(** Source location span. *)
 type t = { file : file_id; start : int; end_ : int }
 
+(** Create span from file ID, start and end positions. *)
 let make file start end_ = { file; start; end_ }
+
+(** Dummy span for testing. *)
 let dummy = { file = 0; start = 0; end_ = 0 }
+
+(** Get file ID. *)
 let file t = t.file
+
+(** Get start position. *)
 let start t = t.start
+
+(** Get end position. *)
 let end_ t = t.end_
+
+(** Get span length. *)
 let len t = t.end_ - t.start
