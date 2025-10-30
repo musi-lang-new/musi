d775e3aadb7d0a6521c690c3143c1452bc4f703c
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-30 06:09:30 +0200

    style: format code for better readability in `memory.ml`

--- musi_rt/memory.ml
+++ musi_rt/memory.ml
@@ -1,20 +1,22 @@
 (** Memory management helpers for heap allocation and ARC. *)
 
 let make_text s =
-  let obj = { Value.refcount = 1; marked = false; data = Value.Text s } in
+  let obj =
+    { Value.refcount = 1; Value.marked = false; Value.data = Value.Text s }
+  in
   Gc.register_object obj;
   Value.HeapRef obj
 
 let text_content = function
-  | Value.HeapRef { data = Value.Text s; _ } -> s
-  | _ -> failwith "not Text value"
+  | Value.HeapRef { Value.data = Value.Text s; _ } -> s
+  | _ -> failwith "not 'Text' value"
 
 let retain = function
-  | Value.HeapRef obj -> obj.refcount <- obj.refcount + 1
+  | Value.HeapRef obj -> obj.Value.refcount <- obj.Value.refcount + 1
   | Value.Unit | Value.Bool _ | Value.Int _ | Value.Nat _ -> ()
 
 let release = function
   | Value.HeapRef obj ->
-    obj.refcount <- obj.refcount - 1;
-    if obj.refcount = 0 then Gc.unregister_object obj
+    obj.Value.refcount <- obj.Value.refcount - 1;
+    if obj.Value.refcount = 0 then Gc.unregister_object obj
   | Value.Unit | Value.Bool _ | Value.Int _ | Value.Nat _ -> ()
