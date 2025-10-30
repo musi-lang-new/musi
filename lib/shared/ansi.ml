6ff9f0b34fe2665a876622eac40a9e9100cf96fd
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-30 09:22:05 +0200

    feat: add bold colour functions for `red`, `yellow`, `cyan`, and `green`

--- lib/shared/ansi.ml
+++ lib/shared/ansi.ml
@@ -10,3 +10,7 @@ let red s = make_color "\027[31m" s
 let green s = make_color "\027[32m" s
 let yellow s = make_color "\027[33m" s
 let cyan s = make_color "\027[36m" s
+let bold_red s = make_color "\027[0;1;31m" s
+let bold_yellow s = make_color "\027[0;1;33m" s
+let bold_cyan s = make_color "\027[0;1;36m" s
+let bold_green s = make_color "\027[0;1;32m" s
