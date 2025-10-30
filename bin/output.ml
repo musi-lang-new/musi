418271dfae84b7d2184cf4e8afe492fefd6188c9
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-10-30 09:28:19 +0200

    refactor: update `status_prefix` to use `Ansi.bold_*` functions for consistent formatting

--- bin/output.ml
+++ bin/output.ml
@@ -1,29 +1,29 @@
-let green s = "\027[32m" ^ s ^ "\027[0m"
-let red s = "\027[31m" ^ s ^ "\027[0m"
-let yellow s = "\027[33m" ^ s ^ "\027[0m"
-let bold s = "\027[1m" ^ s ^ "\027[0m"
-let status_prefix status = bold (Printf.sprintf "%12s" status)
+let status_prefix status = Ansi.bold (Printf.sprintf "%12s" status)
 
 let compiling file =
-  Printf.printf "%s %s\n" (status_prefix (green "Compiling")) file;
+  Printf.printf "%s %s\n" (status_prefix (Ansi.bold_green "Compiling")) file;
   flush stdout
 
-let finished target =
-  Printf.printf "%s %s\n" (status_prefix (green "Finished")) target;
+let finished profile target =
+  Printf.printf
+    "%s %s target -> %s\n"
+    (status_prefix (Ansi.bold_green "Finished"))
+    profile
+    target;
   flush stdout
 
 let checking file =
-  Printf.printf "%s %s\n" (status_prefix (green "Checking")) file;
+  Printf.printf "%s %s\n" (status_prefix (Ansi.bold_green "Checking")) file;
   flush stdout
 
 let error msg =
-  Printf.eprintf "%s %s\n" (status_prefix (red "Error")) msg;
+  Printf.eprintf "%s %s\n" (status_prefix (Ansi.bold_red "Error")) msg;
   flush stderr
 
 let warning msg =
-  Printf.eprintf "%s %s\n" (status_prefix (yellow "Warning")) msg;
+  Printf.eprintf "%s %s\n" (status_prefix (Ansi.bold_yellow "Warning")) msg;
   flush stderr
 
 let running file =
-  Printf.printf "%s %s\n" (status_prefix (green "Running")) file;
+  Printf.printf "%s %s\n" (status_prefix (Ansi.bold_green "Running")) file;
   flush stdout
