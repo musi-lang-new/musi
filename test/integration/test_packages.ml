8676b071258c8b8f0656de1d0515755d83dc92ac
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-01 11:44:35 +0200

    refactor: update `test_import_exported_symbol` to use `compile_and_run_with_package` and add `test_circular_import`

--- test/integration/test_packages.ml
+++ test/integration/test_packages.ml
@@ -4,9 +4,19 @@ let test_import_exported_symbol () =
   let package_source = "const x := 42;\nexport { x };" in
   let main_source = "import { x } from \"lib.ms\";\nx" in
   let exit_code =
-    Helpers.compile_and_run_with_module "lib.ms" package_source main_source
+    Helpers.compile_and_run_with_package "lib.ms" package_source main_source
   in
   check int "import succeeds" 0 exit_code
+
+let test_circular_import () =
+  let a_source = "import { b } from \"b.ms\";\nconst a := 1;\nexport { a };" in
+  let b_source = "import { a } from \"a.ms\";\nconst b := 2;\nexport { b };" in
+  let exit_code =
+    Helpers.compile_and_run_with_packages
+      [ ("a.ms", a_source); ("b.ms", b_source) ]
+      "import { a } from \"a.ms\";\na"
+  in
+  check int "circular import fails" 1 exit_code
 
 let () =
   run
@@ -15,5 +25,6 @@ let () =
       ( "imports"
       , [
           test_case "import-exported-symbol" `Quick test_import_exported_symbol
+        ; test_case "circular-import" `Quick test_circular_import
         ] )
     ]
