3eb41a8b386ac6b249107668d36208fb0e0d5071
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-01 11:46:54 +0200

    chore: rename `test_packages.ml` as `test_modules.ml`

--- test/integration/test_modules.ml
+++ test/integration/test_modules.ml
@@ -1,10 +1,10 @@
 open Alcotest
 
 let test_import_exported_symbol () =
-  let package_source = "const x := 42;\nexport { x };" in
+  let module_source = "const x := 42;\nexport { x };" in
   let main_source = "import { x } from \"lib.ms\";\nx" in
   let exit_code =
-    Helpers.compile_and_run_with_package "lib.ms" package_source main_source
+    Helpers.compile_and_run_with_module "lib.ms" module_source main_source
   in
   check int "import succeeds" 0 exit_code
 
@@ -12,7 +12,7 @@ let test_circular_import () =
   let a_source = "import { b } from \"b.ms\";\nconst a := 1;\nexport { a };" in
   let b_source = "import { a } from \"a.ms\";\nconst b := 2;\nexport { b };" in
   let exit_code =
-    Helpers.compile_and_run_with_packages
+    Helpers.compile_and_run_with_modules
       [ ("a.ms", a_source); ("b.ms", b_source) ]
       "import { a } from \"a.ms\";\na"
   in
@@ -20,7 +20,7 @@ let test_circular_import () =
 
 let () =
   run
-    "Packages"
+    "Modules"
     [
       ( "imports"
       , [
